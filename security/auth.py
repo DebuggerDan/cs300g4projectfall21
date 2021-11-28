088fb9f82c7661d3b2dc83c3157c40f77ae88ec8
Ian10583 <ianguy2000@yahoo.com>
2021-11-27 16:38:43 -0800

    some basic function implementations for checking login info.

--- security/auth.py
+++ security/auth.py
@@ -2,18 +2,35 @@
 # Christopher Juncker, Justin Greever, Samantha Zeigler, Tori Anderson, Naya Mairena, Ian Guy, Dan Jang
 
 
-'''
-planning for functions n stuff here.
 
-int loginGrabber()
-{
-    some kind of function which takes login info, passes it to some sanitizing function in sec, then if that's ok check
-    the database for correct credentials. Maybe have the passing to database be done in another function?
-}
+# planning for functions n stuff here.
+from pkg_resources import get_provider
+from security.sec import loginChecker
 
-int loginDatabaseControl()
-{
-    takes the login info from loginGrabber() and does some database stuff to check if it's ok
-    returns value if login info was found
-}
-'''
+
+def loginGrabber():
+    #take user input for login.
+    username = input('Enter a username: ')
+    #check to make sure it's ok
+    charCheck = loginChecker(username)
+    if charCheck == 3:
+        print("Error: Improper login input!")
+        return -1
+    #passes sanitized entries to database control function
+    loginSuccess = loginDatabaseControl(username, charCheck)
+    if loginSuccess != None:
+        print("Login success... Sending data...\n")
+        return loginSuccess
+    else:
+        print("ID not found. Please try again!")
+        return -2
+
+    #expand below function to run through checking what type (provider / patient / etc) the ID matches
+def loginDatabaseControl(username, charCheck):
+    #does some database stuff, calling get provider / get provider by name depending on charCheck's value
+    ##if charCheck == 1:
+    ##    userData = get_provider_by_name(username)
+    ##    return userData
+    if charCheck == 2:
+        userData = get_provider(username)
+        return userData
--- security/sec.py
+++ security/sec.py
@@ -1,10 +1,10 @@
 # CS 300 - Group (#4) Project: ChocAn [Section: Authentication & Security, subsection: Security] - Fall 2021
 # Christopher Juncker, Justin Greever, Samantha Zeigler, Tori Anderson, Naya Mairena, Ian Guy, Dan Jang
 
-'''
-int loginChecker(int loginData)
-{
-    takes login info from loginGrabber() in auth.py and makes sure that it's a kosher input.
-    stops a classic bobby tables moment from happening
-}
-'''
+def loginChecker(username):
+    if username.isalpha() == True:
+        return 1
+    elif username.isnum() == True:
+        return 2
+    else:
+        return 3
