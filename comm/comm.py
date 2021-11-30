560255e02bcc8244b657d3a950b02da4647f6872
AzerDime <guyian@pdx.edu>
2021-11-29 21:04:43 -0800

    comments added to comm

---
+++ comm/comm.py
@@ -0,0 +1,20 @@
+"""
+comm comments - all functions assume user is "logged in" already
+make sure the "logged in" person meets requirements
+
+member report function - prints report - manager only
+    take member id
+    displays information for that member
+    displays services from this week for member
+    if desired, display older services
+provider report function - prints report - manager only
+    same as above, but this time for providers
+summary report function - prints report - manager only
+    lists all members who have received services in past week
+    lists all providers who have rendered services in past week
+    lists (if desired) details on rendered services
+    display total price of services rendered
+weekly summary function - prints report - manager and provider access
+    same as above, but only iterates once a week.
+    note: if in middle of week, do not update
+"""
