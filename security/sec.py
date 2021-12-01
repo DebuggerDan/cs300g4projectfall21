4fe82327a80c7eb72b95c3427d8c11ad0737abf7
Ian10583 <ianguy2000@yahoo.com>
2021-11-30 21:00:37 -0800

    testing merging my branch into main. this might break things, so yell at me if it does!

--- security/sec.py
+++ security/sec.py
@@ -1,5 +1,7 @@
 # CS 300 - Group (#4) Project: ChocAn [Section: Authentication & Security, subsection: Security] - Fall 2021
 # Christopher Juncker, Justin Greever, Samantha Zeigler, Tori Anderson, Naya Mairena, Ian Guy, Dan Jang
+import sys
+
 
 def loginChecker(username):
     if username.isalpha() == True:
